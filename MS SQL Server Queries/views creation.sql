1b5051f873dece4056f6dc71a486e043c9fa52a9
Anugunj Barange <barangeanugunj26@gmail.com>
2025-08-06 14:04:02 +0530

    Add files via upload

---
+++ MS SQL Server Queries/views creation.sql
@@ -0,0 +1,8 @@
+use db_Churn
+
+
+Create View vw_ChurnData as 
+	Select * from prod_Churn where Customer_Status In ('Churned','Stayed')
+
+Create View vw_JoinData as
+	select * from prod_Churn where Customer_Status = 'Joined'
